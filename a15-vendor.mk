ed5549811f98ef80a49e9e5cb9f5f8a26ecd6f82
Llucs <c307lucas@gmail.com>
2025-12-24 15:26:16 -0300

    Vendor: Adicionando definições de PRODUCT e MODEL

--- a15-vendor.mk
+++ a15-vendor.mk
@@ -1,1 +1,8 @@
+# Arquivo gerado para o Samsung Galaxy A15 (SM-A155M)
+
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := a15
+PRODUCT_MODEL := SM-A155M
+
 $(call inherit-product, vendor/samsung/a15/a15-vendor-blobs.mk)
